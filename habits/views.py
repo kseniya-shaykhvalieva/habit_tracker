c6511a1b2341c2be24cd1bba4886c5c2f59f6ad1
kseniya-shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-07-07 12:33:04 +0500

    Написаны контроллеры и пагинатор для модели привычек.

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from habits.models import Habit
+from habits.paginations import HabitPagination
+from habits.serializers import HabitSerializer
+from users.permissions import IsOwner
 
-# Create your views here.
+
+class HabitViewSet(ModelViewSet):
+    """ViewSet для CRUD привычек."""
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = HabitPagination
+
+    def get_queryset(self):
+        """Возвращает только привычки текущего пользователя."""
+        return Habit.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        """При создании привязывает привычку к текущему пользователю."""
+        serializer.save(user=self.request.user)
+
+    def get_permissions(self):
+        """Назначает права: для просмотра/редактирования/удаления — только владелец."""
+        if self.action in ['retrieve', 'update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsAuthenticated, IsOwner]
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
+
+
+class PublicHabitListView(ListAPIView):
+    """Эндпоинт для списка публичных привычек."""
+    queryset = Habit.objects.filter(is_public=True)
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = HabitPagination
